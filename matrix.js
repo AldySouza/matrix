93d3449d4a89088e1180d56597ec08563fce30e6
Aldy Souza <60198926+AldySouza@users.noreply.github.com>
2022-01-12 11:48:53 -0300

    update

--- matrix.js
+++ matrix.js
@@ -1,11 +1,22 @@
-
-
-
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 let cw = window.innerWidth;
 let ch = window.innerHeight;
+
+
+canvas.width = cw;
+canvas.height = ch;
+
+
+window.addEventListener('resize', function(event) {
+    cw = window.innerWidth;
+    ch = window.innerHeight;
+    canvas.width = cw
+    canvas.height = ch;
+    maxColumns = cw / fontSize;
+    console.log(cw, ch)
+}, true);
 
 let charArr = [
   "a",
@@ -71,14 +82,13 @@ let charArr = [
   "Ц",
 ];
 
-let maxCharCount = 1000;
+let maxCharCount = 300;
 let fallingCharArr = [];
-let fontSize = 15;
+let fontSize = 13;
 let maxColumns = cw / fontSize;
-canvas.width = cw;
-canvas.height = ch;
 
-let frames = 0
+
+let frames = 0;
 
 class FallingChar {
   constructor(x, y) {
@@ -87,34 +97,34 @@ class FallingChar {
   }
 
   draw(ctx) {
-    this.value = 
+    this.value =
       charArr[Math.floor(Math.random() * (charArr.length - 1))].toUpperCase();
-    this.speed = (Math.random() * fontSize * 3) /4 + (fontSize * 3) / 4;
+    this.speed = (Math.random() * fontSize * 3) / 4 + (fontSize * 3) / 4;
 
-    ctx.fillStyle = "rgb(0,255,0)"
-    ctx.font = fontSize + "px san-serif"
-    ctx.fillText(this.value, this.x, this.y)
+    ctx.fillStyle = "rgba(0,255,0)";
+    ctx.font = fontSize + "px sans-serif";
+    ctx.fillText(this.value, this.x, this.y);
     this.y += this.speed;
 
-    if(this.y > ch) {
+    if (this.y > ch) {
       this.y = (Math.random() * ch) / 2 - 50;
       this.x = Math.floor(Math.random() * maxColumns) * fontSize;
-      this.speed = (-Math.random() * fontSize * 3) / 4 + (fontSize * 3) / 4; 
+      this.speed = (-Math.random() * fontSize * 3) / 4 + (fontSize * 3) / 4;
     }
   }
 }
 
 let update = () => {
-  if(fallingCharArr.length < maxCharCount) {
+  if (fallingCharArr.length < maxCharCount) {
     let fallingChar = new FallingChar(
-      Math.floor(Math.random() * maxColumns) * fontSize, 
+      Math.floor(Math.random() * maxColumns) * fontSize,
       (Math.random() * ch) / 2 - 50
     );
     fallingCharArr.push(fallingChar);
   }
   ctx.fillStyle = "rgba(0,0,0,0.05)";
   ctx.fillRect(0, 0, cw, ch);
-  for(let i = 0; i < fallingCharArr.length && frames % 2 == 0; i++) {
+  for (let i = 0; i < fallingCharArr.length && frames % 2 == 0; i++) {
     fallingCharArr[i].draw(ctx);
   }
 
@@ -122,4 +132,4 @@ let update = () => {
   frames++;
 };
 
-updade();
+update();
